c7b047dc3961c5187aa9762002bd6abf9e7a820d
LT <leetoplayaz@yandex.ru>
2021-05-14 14:18:02 +0300

    - changed filters height

--- src/views/components/listing/partials/filters.blade.php
+++ src/views/components/listing/partials/filters.blade.php
@@ -22,11 +22,11 @@
                  style="display: none;"></div>
 
             <div x-show="dropdownOpen"
-                 class="absolute right-0 w-64 px-6 py-6 mt-2 bg-white rounded-md overflow-y-auto h-64 shadow-xl z-10"
+                 class="absolute right-0 w-64 px-6 py-6 mt-2 bg-white rounded-md overflow-y-auto h-96 shadow-xl z-10"
                  style="display: none;">
 
                 <form class="w-full max-w-sm" action="{{ $resource->route("index") }}" method="get">
-                    {{ csrf_field() }}
+                    @csrf
 
                     @foreach($resource->filters() as $filter)
                         <div class="mb-4">
