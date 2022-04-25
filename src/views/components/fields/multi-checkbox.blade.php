9cc140e8c8fae122e84b7cdb975afa0888733e50
LT <leetoplayaz@yandex.ru>
2022-04-25 12:54:41 +0300

    fixes

--- src/views/components/fields/multi-checkbox.blade.php
+++ src/views/components/fields/multi-checkbox.blade.php
@@ -12,7 +12,6 @@
 
             @if(isset($attr['pivotField']) && $attr['pivotField'] != '')
                 <input type="text"
-                       required
                        name="{{ $attr['originalName'] }}_{{ $attr['pivotField'] }}[]"
                        class="bg-white focus:outline-none focus:shadow-outline border border-gray-300 rounded-lg py-2 px-4  ml-2 appearance-none leading-normal"
                        value="{{ $value->firstWhere('id', '=', $optionValue)->pivot->{$attr['pivotField']} ?? '' }}"
--- src/views/components/partials/listing_item.blade.php
+++ src/views/components/partials/listing_item.blade.php
@@ -1,4 +1,11 @@
 <div class="flex items-center">
-    <img src="{{ $image }}" class="w-8 h-8 object-cover rounded-full" alt="{{ $value }}">
-    <a class="text-gray-700 text-sm mx-3" href="{{ $link }}">{{ $value }}</a>
+    @if($image)
+        <img src="{{ $image }}" class="w-8 h-8 object-cover rounded-full" alt="{{ $value }}">
+    @endif
+
+    @if($link)
+        <a class="text-gray-700 text-sm mx-3" href="{{ $link }}">{{ $value }}</a>
+    @else
+        <span class="text-gray-700 text-sm mx-3">{{ $value }}</span>
+    @endif
 </div>
