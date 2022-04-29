634aabc8665889d4911f5952535db891b9ff6406
LT <leetoplayaz@yandex.ru>
2022-04-29 11:50:09 +0300

    improve belongsToMany (multi-checkbox)

--- src/views/components/fields/multi-checkbox.blade.php
+++ src/views/components/fields/multi-checkbox.blade.php
@@ -2,26 +2,43 @@
     @foreach($attr["options"] as $optionValue => $optionName)
         <div>
             <input @if($value->contains("id", "=", $optionValue)) checked @endif
-            id="{{ $attr["name"] }}_{{ $optionValue }}"
+            id="{{ $attr["originalName"] ?? $attr["name"] }}_{{ $optionValue }}"
                    type="checkbox" name="{{ $attr["name"] }}"
                    value="{{ $optionValue }}"
             />
+
             @if(isset($label))
                 <label class="ml-5" for="{{ $attr["name"] }}_{{ $optionValue }}">{{ $optionName }}</label>
             @endif
 
             @if(isset($attr['fields']))
-                @foreach($attr["fields"] as $field)
-                    <div class="my-4">
-                        {{ $resource->component(
-                        $field,
-                        "fields",
-                        $value->firstWhere('id', '=', $optionValue)->pivot ?? $emptyValue,
-                        true)
-                        }}
-                    </div>
-                @endforeach
+                <div id="{{ $attr["originalName"] ?? $attr["name"] }}_{{ $optionValue }}_pivots">
+                    @foreach($attr["fields"] as $field)
+                        <div class="my-4">
+                            {{ $resource->component(
+                            $field,
+                            "fields",
+                            $value->firstWhere('id', '=', $optionValue)->pivot ?? $emptyValue,
+                            true)
+                            }}
+                        </div>
+                    @endforeach
+                </div>
             @endif
         </div>
+
+        <script>
+          let input_{{ $attr["originalName"] ?? $attr["name"] }} = document.querySelector("#{{ $attr["originalName"] ?? $attr["name"] }}_{{ $optionValue }}");
+
+          let pivotsDiv_input_{{ $attr["originalName"] ?? $attr["name"] }} = document.querySelector("#{{ $attr["originalName"] ?? $attr["name"] }}_{{ $optionValue }}_pivots");
+
+          let inputs_{{ $attr["originalName"] ?? $attr["name"] }} = pivotsDiv_input_{{ $attr["originalName"] ?? $attr["name"] }}.querySelectorAll('input, textarea, select');
+
+          inputs_{{ $attr["originalName"] ?? $attr["name"] }}.forEach(function(value, key) {
+            value.addEventListener('input', (event) => {
+              input_{{ $attr["originalName"] ?? $attr["name"] }}.checked = event.target.value;
+            });
+          })
+        </script>
     @endforeach
 </div>
